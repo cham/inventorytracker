14238721c203acfdfec41b8be95078a9d029143a
Dan Neame <danneame@gmail.com>
2018-03-07 12:28:34 +0000

    db/index linting

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,10 @@
 import * as NeDB from 'nedb'
 import path from 'path'
 
+console.log(NeDB)
+
 /* eslint-disable new-cap */
-const db = new NeDB.default({
+const db = new NeDB({
   filename: path.join(__dirname, '../../database.db')
 })
 /* eslint-enable new-cap */
@@ -17,85 +19,94 @@ const getConnection = () => new Promise((resolve, reject) => {
 })
 
 export const insert = docs => new Promise((resolve, reject) => {
-  getConnection().then(() => {
-    db.insert(docs, (err, newDoc) => {
-      if (err) {
-        return reject(err)
-      }
-      resolve(newDoc)
+  getConnection()
+    .then(() => {
+      db.insert(docs, (err, newDoc) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve(newDoc)
+      })
     })
-  })
-  .catch(reject)
+    .catch(reject)
 })
 
 export const find = filter => new Promise((resolve, reject) => {
-  getConnection().then(() => {
-    db.find(filter, (err, docs) => {
-      if (err) {
-        return reject(err)
-      }
-      resolve(docs)
+  getConnection()
+    .then(() => {
+      db.find(filter, (err, docs) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve(docs)
+      })
     })
-  })
-  .catch(reject)
+    .catch(reject)
 })
 
 export const findOne = filter => new Promise((resolve, reject) => {
-  getConnection().then(() => {
-    find(filter)
-      .then(docs => resolve(docs[0]))
-      .catch(reject)
-  })
-  .catch(reject)
+  getConnection()
+    .then(() => {
+      find(filter)
+        .then(docs => resolve(docs[0]))
+        .catch(reject)
+    })
+    .catch(reject)
 })
 
 export const patch = (filter, patchSet) => new Promise((resolve, reject) => {
-  getConnection().then(() => {
-    db.update(filter, { $set: patchSet }, {}, (err, docs) => {
-      if (err) {
-        return reject(err)
-      }
-      resolve(docs)
+  getConnection()
+    .then(() => {
+      db.update(filter, { $set: patchSet }, {}, (err, docs) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve(docs)
+      })
     })
-  })
-  .catch(reject)
+    .catch(reject)
 })
 
 export const update = (filter, newDoc) => new Promise((resolve, reject) => {
-  getConnection().then(() => {
-    db.update(filter, newDoc, {}, (err, docs) => {
-      if (err) {
-        return reject(err)
-      }
-      resolve(docs)
+  getConnection()
+    .then(() => {
+      db.update(filter, newDoc, {}, (err, docs) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve(docs)
+      })
     })
-  })
-  .catch(reject)
+    .catch(reject)
 })
 
 export const insertOrPatch = (filter, patchSet) => new Promise((resolve, reject) => {
-  getConnection().then(() => {
-    find(filter)
-      .then((results) => {
-        if (results && results.length) {
-          return patch(filter, patchSet)
+  getConnection()
+    .then(() => {
+      find(filter)
+        .then((results) => {
+          if (results && results.length) {
+            return patch(filter, patchSet)
+              .then(resolve)
+              .catch(reject)
+          }
+          return insert(patchSet)
             .then(resolve)
             .catch(reject)
-        }
-        return insert(patchSet)
-          .then(resolve)
-          .catch(reject)
-      })
-  })
+        })
+    })
+    .catch(reject)
 })
 
 export const remove = (filter) => new Promise((resolve, reject) => {
-  getConnection().then(() => {
-    db.remove(filter, { multi: true }, function (err) {
-      if (err) {
-        return reject(err)
-      }
-      resolve()
-    });
-  })
+  getConnection()
+    .then(() => {
+      db.remove(filter, { multi: true }, function (err) {
+        if (err) {
+          return reject(err)
+        }
+        resolve()
+      })
+    })
+    .catch(reject)
 })
