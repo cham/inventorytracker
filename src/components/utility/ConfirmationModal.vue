4a3abffddfe407c86274e24867841f389031934b
Dan Neame <danneame@gmail.com>
2018-03-07 14:30:49 +0000

    Include uuidv4 and utility components from battletracker

---
+++ src/components/utility/ConfirmationModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <modal @cancel="$emit('cancel')">
+    <h3 slot="header">Confirm</h3>
+    <div slot="body">
+      <p>
+        {{message}}
+      </p>
+      <p>
+        <button @click="$emit('confirm')">Confirm</button>
+        <button @click="$emit('cancel')">Cancel</button>
+      </p>
+    </div>
+  </modal>
+</template>
+
+<script>
+import Modal from './Modal'
+
+export default {
+  components: {
+    Modal
+  },
+  props: ['message'],
+  methods: {
+    onKeyPress (e) {
+      if (e.key === 'Enter') {
+        this.$emit('confirm')
+      }
+    }
+  },
+  created () {
+    window.addEventListener('keyup', this.onKeyPress)
+  },
+  beforeDestroy () {
+    window.removeEventListener('keyup', this.onKeyPress)
+  }
+}
+</script>
