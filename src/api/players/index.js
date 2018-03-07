93674d7bae050209771c110ef665c246128c590a
Dan Neame <danneame@gmail.com>
2018-03-07 15:37:47 +0000

    Add players API methods for getting, adding and deleting players

---
+++ src/api/players/index.js
@@ -0,0 +1,29 @@
+import * as db from '../../db'
+import { generateId } from '../../utils/uuidv4'
+
+export const getPlayers = () => db.findOne({ players: { $exists: true } })
+  .then((playersDoc) => {
+    if (playersDoc && playersDoc.players && playersDoc.players.length) {
+      return playersDoc.players
+    }
+    return []
+  })
+
+export const addPlayer = player => {
+  const playerData = {
+    ...player,
+    id: generateId(),
+    inventory: [],
+    totalWeight: 0
+  }
+  return getPlayers()
+    .then(players => players.concat(playerData))
+    .then(players => db.insertOrPatch({ players: { $exists: true } }, { players }))
+    .then(() => playerData)
+}
+
+export const deletePlayer = playerId => {
+  return getPlayers()
+    .then(players => players.filter(player => player.id !== playerId))
+    .then(players => db.insertOrPatch({ players: { $exists: true } }, { players }))
+}
