7653fa0033ddb05f38755088f2869381c66e0f96
Dan Neame <danneame@gmail.com>
2018-03-07 11:48:30 +0000

    Toggleable panels for Players and Items

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,53 @@
 <template>
   <div id="app">
+    <header>
+      <button @click="onClickPlayers" class="toggle-players">Players</button>
+      <button @click="onClickItems" class="toggle-items">Items</button>
+    </header>
+    <section ref="players" class="content-area players">
+    </section>
+    <section ref="items" class="content-area items">
+    </section>
   </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    onClickPlayers () {
+      this.$refs.players.style.left = '0%'
+      this.$refs.items.style.left = '100%'
+    },
+    onClickItems () {
+      this.$refs.players.style.left = '-100%'
+      this.$refs.items.style.left = '0%'
+    }
+  }
 }
 </script>
 
 <style scoped>
+#app {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow: hidden;
+}
+.content-area {
+  position: absolute;
+  top: 52px;
+  bottom: 0;
+  transition: left ease-out 0.2s;
+  width: 100%;
+}
+.players {
+  background: red;
+  left: 0;
+}
+.items {
+  background: #fef;
+  left: 100%;
+}
 </style>
