c953e2992fb7c9df958139b94387ac5e13a47b8a
Dan Neame <danneame@gmail.com>
2018-03-07 12:10:39 +0000

    Add API stub

---
+++ src/api/index.js
@@ -0,0 +1,4 @@
+// awaiting https://tc39.github.io/proposal-export-ns-from/
+import * as items_ from './items'
+
+export const items = items_
---
+++ src/api/items/index.js
@@ -0,0 +1,5 @@
+import * as db from '../../db'
+
+export const getItems = () => new Promise((resolve, reject) => {
+
+})
