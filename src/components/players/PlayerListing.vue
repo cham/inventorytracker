94db12d122caccb867f00214b533986e5acd6cca
Dan Neame <danneame@gmail.com>
2018-03-07 15:40:02 +0000

    Show player encumbrance type and available movement based on player strength and total weight

--- src/components/players/PlayerListing.vue
+++ src/components/players/PlayerListing.vue
@@ -5,9 +5,11 @@
         <tr>
           <th>Name</th>
           <th>Strength</th>
-          <th>Movement</th>
+          <th>Base Movement</th>
           <th>Total Weight</th>
-          <th>Speed</th>
+          <th>Encumbrance</th>
+          <th>Movement</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
@@ -16,20 +18,69 @@
           <td>{{player.strength}}</td>
           <td>{{player.movement}}</td>
           <td>{{player.totalWeight}}</td>
-          <td>{{player.speed}}</td>
+          <td>{{playerEncumbranceType(player)}}</td>
+          <td>{{playerAvailableMovement(player)}}</td>
+          <td>
+            <button>Edit</button>
+            <button @click="confirmDeletePlayer = true;idToDelete = player.id">Delete</button>
+          </td>
         </tr>
       </tbody>
     </table>
+    <confirmation-modal v-if="confirmDeletePlayer"
+      message="This will permanently delete this player."
+      @confirm="confirmDeletePlayer = false;doDeletePlayer()"
+      @cancel="confirmDeletePlayer = false;idToDelete = null"
+    ></confirmation-modal>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import ConfirmationModal from '../utility/ConfirmationModal'
 
 export default {
-  computed: mapGetters({
-    players: 'players'
-  })
+  components: {
+    ConfirmationModal
+  },
+  data () {
+    return {
+      confirmDeletePlayer: false
+    }
+  },
+  computed: {
+    ...mapGetters({
+      players: 'players'
+    })
+  },
+  methods: {
+    doDeletePlayer () {
+    },
+    playerEncumbranceType (player) {
+      if (player.totalWeight >= player.strength * 15) {
+        return 'Immobile'
+      }
+      if (player.totalWeight >= player.strength * 10) {
+        return 'Heavily Encumbered'
+      }
+      if (player.totalWeight >= player.strength * 5) {
+        return 'Encumbered'
+      }
+      return 'Unencumbered'
+    },
+    playerAvailableMovement (player) {
+      if (player.totalWeight >= player.strength * 15) {
+        return 0
+      }
+      if (player.totalWeight >= player.strength * 10) {
+        return player.speed - 20
+      }
+      if (player.totalWeight >= player.strength * 5) {
+        return player.speed - 10
+      }
+      return player.speed
+    }
+  }
 }
 </script>
 
